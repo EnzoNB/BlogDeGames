893c0779ffe367a5c916919b636d6e0279b60253
Enzo Nogueira Barrese <enzobarrese@usp.br>
2023-11-24 18:33:15 -0300

    Branch CAtegories | Commit 1

--- blogdegames/urls.py
+++ blogdegames/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import HomeView, ViewDetalhadaPost, ViewAdicionarPost, ViewAtualizarPost, ViewApagarPost, ViewAdicionarComentario
+from .views import HomeView, ViewDetalhadaPost, ViewAdicionarPost, ViewAtualizarPost, ViewApagarPost, ViewAdicionarComentario, CategoryView, ViewTodasCategorias
 
 urlpatterns = [
     path('',HomeView.as_view(),name="home"),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('post/edit/<int:pk>', ViewAtualizarPost.as_view(), name = "update_post"),
     path('post/delete/<int:pk>', ViewApagarPost.as_view(), name = "delete_post"),
     path('post/<int:pk>/comentando/', ViewAdicionarComentario.as_view(), name = "add_comment"),
+    path('categoria/<str:types>', CategoryView, name = "Category"),
+    path('categorias/', ViewTodasCategorias, name = "lista_categorias"),
 ]
