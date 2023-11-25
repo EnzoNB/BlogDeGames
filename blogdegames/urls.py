c93aa582278d12c5a51a231e4063d89edc8afc42
Enzo Nogueira Barrese <enzobarrese@usp.br>
2023-11-25 16:10:46 -0300

    Commit Extra, Correção De Bugs

--- blogdegames/urls.py
+++ blogdegames/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import HomeView, ViewDetalhadaPost, ViewAdicionarPost, ViewAtualizarPost, ViewApagarPost
+from .views import HomeView, ViewDetalhadaPost, ViewAdicionarPost, ViewAtualizarPost, ViewApagarPost, ViewAdicionarComentario, CategoryView, ViewTodasCategorias
 
 urlpatterns = [
     path('',HomeView.as_view(),name="home"),
@@ -7,4 +7,7 @@ urlpatterns = [
     path('posting/', ViewAdicionarPost.as_view(), name = "add_post"),
     path('post/edit/<int:pk>', ViewAtualizarPost.as_view(), name = "update_post"),
     path('post/delete/<int:pk>', ViewApagarPost.as_view(), name = "delete_post"),
+    path('post/<int:pk>/comentando/', ViewAdicionarComentario.as_view(), name = "add_comment"),
+    path('categoria/<str:types>', CategoryView, name = "Category"),
+    path('categorias/', ViewTodasCategorias, name = "lista_categorias"),
 ]
